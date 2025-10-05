82435bbae0baf65701df5c9502626c759a30483b
Harsh-Wardhan-404 <harshwardhansaindane@gmail.com>
2025-10-05 19:48:02 +0530

    gradient

---
+++ frontend/tailwind.config.js
@@ -0,0 +1,6 @@
+// /** @type {import('tailwindcss').Config} */
+// module.exports = {
+//     content: ['./src/**/*.{js,ts,jsx,tsx}'],
+//     corePlugins: { preflight: false },      // keep your existing CSS intact
+//     // prefix: 'tw-',                        // uncomment to prefix classes (e.g., tw-flex)
+//   };
